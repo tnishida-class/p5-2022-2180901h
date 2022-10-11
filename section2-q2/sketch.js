c8c903213f76d88a08216da265f1484875ed5d4c
2180901h <114969230+2180901h@users.noreply.github.com>
2022-10-11 16:32:33 +0900

    Change section2-2q

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -5,8 +5,8 @@ function setup() {
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
-      if(size*(i+j) / 2 % 1 )
-      {fill(100, 100, 100); rect(size * i, size * j, size, size);
+      if(size*(i + j) / 2 % 1 ){
+        fill(100, 100, 100); rect(size * i, size * j, size, size);
         if(size*j<75){
           fill(255, 0, 0,); stroke(100, 100, 100); ellipse(size * i + 25/2, size * j + 25/2, size * 9 / 10)
         };
